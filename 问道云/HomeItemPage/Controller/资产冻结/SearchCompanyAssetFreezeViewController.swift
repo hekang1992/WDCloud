ff7cec67d4c67230098d1aef476ba163757e1431
何康 <hekang19921111@hotmail.com>
2025-02-12 21:14:59 +0800

    add:新增公告,司法拍卖,贷款逾期

---
+++ 问道云/HomeItemPage/Controller/资产冻结/SearchCompanyAssetFreezeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchCompanyAssetFreezeViewController.swift
+//  问道云
+//
+//  Created by 何康 on 2025/2/12.
+//
+
+import UIKit
+
+class SearchCompanyAssetFreezeViewController: WDBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
